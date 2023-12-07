e1f96d8e07b367789bc6609a4a314b110a92d64c
prateekrajgautam <prateekrajgautam@gmail.com>
2023-12-07 11:47:34 +0530

    spacegame init without sidebar

---
+++ main.js
@@ -0,0 +1,31 @@
+const loadSkelton =()=> {
+  append(app, gen(header, "header", "", "header"), "o");
+  append(header, gen(nav, "nav", gen(ul,"navlist","", "nav")));
+  append(header,gen(h1,"GameName","Space Game"),'b')
+  append(app, gen(main, "main", gen(section,"hero","","section"), "main"));
+  append(app, gen(footer, "footer", "", "footer"));
+
+
+}
+
+loadSkelton()
+
+load(["./md.scss"]);
+
+
+addEventListener("keydown",(e)=>{
+  if("w,a,s,d,up,down,left,right".split(",").includes(e.key)){
+    grab("#GameName")[0].classList.add("blur")
+  }
+})
+
+
+addEventListener("keyup",(e)=>{
+    grab("#GameName")[0].classList.remove("blur")
+})
+
+
+$$.init();
+
+
+w
